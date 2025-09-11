60eeb8cc8150cf56dfd32e0677cb057bf0f11d22
dugedog <dugedog@gmail.com>
2025-09-11 22:07:54 +1000

    CH2.L6 More Refactoring

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from stats import get_book_letters
-from stats import get_book_words
+from stats import get_book_letters, get_book_words
 
 
 def main ():
-
-    get_book_words("/home/drexes/workspace/github.com/dugedog/bookbot/books/frankenstein.txt")
-    num_letters = get_book_letters("/home/drexes/workspace/github.com/dugedog/bookbot/books/frankenstein.txt")
-    print (num_letters)
+    book_path = "books/frankenstein.txt"
+    book_contents = read_file (book_path)
+    num_words = get_book_words(book_contents)
+    num_letters = get_book_letters(book_contents)
+    print(f"{num_words} found in document")
+    print(num_letters)
+    
+    
+    
+def read_file (file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return (file_contents)
     
 main ()
--- stats.py
+++ stats.py
@@ -1,29 +1,17 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return print(file_contents)
-
-def get_book_words(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        num_words = file_contents.split()
-    return print(f"{len(num_words)} words found in the document")
+def get_book_words(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    return num_words
 
 
-def get_book_letters(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
+def get_book_letters(file_contents):
     file_contents = file_contents.lower()
     count = {}
-
     for letter in file_contents:
         if count.get(letter,"Invalid") == "Invalid":
             count.update({letter : 1})
         else:
-            increment = count.get(letter) 
-            increment += 1
-            count.update({letter : increment}) 
-            
-    return print (count)
+            count[letter] +=1        
+    return count
 
 
