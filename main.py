1b5819df1e06a352d569a08091ad3cc2f0ab7250
dugedog <dugedog@gmail.com>
2025-09-11 17:57:51 +1000

    CH2.L6 Count Characters-COMPLETE

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from stats import get_book_letters
 from stats import get_book_words
 
-def main ():
-    get_book_words("/home/drexes/workspace/github.com/dugedog/bookbot/books/frankenstein.txt")
 
+def main ():
+
+    get_book_words("/home/drexes/workspace/github.com/dugedog/bookbot/books/frankenstein.txt")
+    num_letters = get_book_letters("/home/drexes/workspace/github.com/dugedog/bookbot/books/frankenstein.txt")
+    print (num_letters)
+    
 main ()
