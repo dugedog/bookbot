3beb42ab793c250a9928ad1ad399e3fbeebc8f85
dugedog <dugedog@gmail.com>
2025-09-12 23:11:08 +1000

    CH3.L3 - Submit Link - COMPLETE

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from stats import get_book_letters, get_book_words, sort_list
-
+import sys
 
 def main ():
+    if len(sys.argv)<2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     print(f"Analyzing book found at {book_path}...")
     book_contents = read_file (book_path)
     num_words = get_book_words(book_contents)
@@ -13,7 +16,7 @@ def main ():
     num_letters = get_book_letters(book_contents)
     sort_list(num_letters) 
     print("============= END ===============")
-    
+        
 def read_file (file_path):
     with open(file_path) as f:
         file_contents = f.read()
