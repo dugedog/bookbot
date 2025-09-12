d4a152a23a4b9c87a9638bbd70569b2eb57abc06
dugedog <dugedog@gmail.com>
2025-09-12 22:50:01 +1000

    CH3.L1 Print a Report-COMPLETE

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from stats import get_book_letters, get_book_words, sort_list
 
 
 def main ():
+    print("============ BOOKBOT ============")
     book_path = "books/frankenstein.txt"
+    print(f"Analyzing book found at {book_path}...")
     book_contents = read_file (book_path)
     num_words = get_book_words(book_contents)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     num_letters = get_book_letters(book_contents)
-    print(f"{num_words} found in document")
-    #print(num_letters)
-    print(sort_list(num_letters))    
-    
+    sort_list(num_letters) 
+    print("============= END ===============")
     
 def read_file (file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def sort_on(items):
+    return items["num"]
+
 def get_book_words(file_contents):
     words = file_contents.split()
     num_words = len(words)
@@ -20,6 +23,8 @@ def sort_list(unsorted_list):
         if pair.isalpha()==False:
             continue
         sorted_list.append({"char":pair, "num": unsorted_list.get(pair)})
-        sorted_list.sort(reverse=True, key="num")
-    return(sorted_list)
+        sorted_list.sort(reverse=True, key=sort_on)
+    for pair in sorted_list:
+        print(f"{pair["char"]}: {pair["num"]}")
+    return
    
